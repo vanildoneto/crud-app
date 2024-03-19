4efddabf26a3a06459ed39b493ff1deb485b9616
Vanildo José <netojose457@gmail.com>
2024-03-18 22:12:55 -0300

    criando tela de home e fazendo link com banco de dados firebase

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,3 +5,58 @@ function logout() {
         alert('Erro ao fazer logout !')
     })
 }
+
+firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+        procurarTransacoes(user)
+    }
+});
+
+function procurarTransacoes(user) {
+    loading();
+    firebase.firestore()
+        .collection('transacoes')
+        .where('user.uid', '==', user.uid)
+        .orderBy('date', 'desc')
+        .get()
+        .then(snapshot => {
+        esconderLoading();
+        const transacoes = snapshot.docs.map(doc => doc.data());
+        adicionarTransacoes(transacoes)
+    })
+    .catch(error => {
+        esconderLoading();
+        alert('Erro ao recuperar transações :( ')
+    })
+}
+
+function adicionarTransacoes(transacao) {
+    const listaOrdenada = document.getElementById('transacoes');
+
+    transacao.forEach(element => {
+        const li = document.createElement('li');
+        li.classList.add(element.type);
+
+        const date = document.createElement('p');
+        date.innerHTML = formatarData(element.date);
+        li.appendChild(date);
+
+        const dinheiro = document.createElement('p');
+        dinheiro.innerHTML = formatarDinheiro(element.dinheiro);
+        li.appendChild(dinheiro);
+
+        const tipoTransacao = document.createElement('p');
+        tipoTransacao.innerHTML = element.tipoTransacao;
+        li.appendChild(tipoTransacao);
+
+        listaOrdenada.appendChild(li)
+    });
+}
+
+function formatarData(date) {
+    return new Date(date).toLocaleDateString('pt-br');
+}
+
+function formatarDinheiro(dinheiro) {
+    return `${dinheiro.moeda} ${dinheiro.valor.toFixed(2)}`
+}
